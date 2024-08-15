1d1af253dfa9df50ec621ae3141c5771b7408604
Eli Holderness <eli.miriam.holderness@gmail.com>
2024-08-15 16:24:41 +0100

    Update app.py to save data to DB

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+import sqlite3
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+
+app.secret_key = "secret key"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -9,5 +12,21 @@ def index():
    else:
        name = request.form.get('name')
        number = request.form.get('number')
-       print(f'you clicked the button with name {name} and number {number}!')
+       if not all((name, number)):
+           flash("Please enter both a name and a phone number.")
+       else:
+           print(f"You clicked the button with name {name} and number {number}!")
+           add_new_person_to_db(name, number)
+           flash(f"You added {name} with number {number}!")
    return render_template('app.html')
+
+
+def add_new_person_to_db(name, number):
+   conn = sqlite3.connect('database.db')
+   cur = conn.cursor()
+   cur.execute(f"""
+      INSERT INTO people (name, phone_number) VALUES
+         ('{name}', '{number}')"""
+         )
+   conn.commit()
+   conn.close()
