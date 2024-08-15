1909e4f1db4db8af47a4886c8cdb020f10b5bbcf
Eli Holderness <eli.miriam.holderness@gmail.com>
2024-08-15 14:55:04 +0100

    Step 1 complete

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'hello world'
+   if request.method == 'GET':
+       pass
+   else:
+       name = request.form.get('name')
+       number = request.form.get('number')
+       print(f'you clicked the button with name {name} and number {number}!')
+   return render_template('app.html')
