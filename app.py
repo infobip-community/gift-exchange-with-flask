c70fff00ff1b19fc93729b287d352fd10067de6a
Eli Holderness <eli.miriam.holderness@gmail.com>
2024-08-22 14:47:22 +0100

    Step 3: add verification of phone numbers

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import sqlite3
+import os
 from flask import Flask, render_template, request, flash
+from infobip_channels.sms.channel import SMSChannel
 
 app = Flask(__name__)
 
 app.secret_key = "secret key"
+tfa_application_id = os.environ['TFA_APP_ID']
+tfa_template_id = os.environ['TFA_TEMPLATE_ID']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,6 +21,7 @@ def index():
        else:
            print(f"You clicked the button with name {name} and number {number}!")
            add_new_person_to_db(name, number)
+           send_verification_text(number)
            flash(f"You added {name} with number {number}!")
    return render_template('app.html')
 
@@ -30,3 +35,66 @@ def add_new_person_to_db(name, number):
          )
    conn.commit()
    conn.close()
+
+def send_verification_text(number):
+    channel = SMSChannel.from_env()
+    response = channel.send_pin_over_sms(
+        {"ncNeeded": "false"},
+        {
+            "applicationId": tfa_application_id,
+            "messageId": tfa_template_id,
+            "from": "InfoSMS",
+            "to": number,
+        }
+    )
+    save_pin_id_for_number(number, response.pin_id)
+
+
+def save_pin_id_for_number(number, pin_id):
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute(f"""
+        UPDATE people
+        SET verification_code = '{pin_id}'
+        WHERE phone_number = {number};"""
+    )
+    conn.commit()
+    conn.close()
+
+@app.route('/verify', methods=['GET', 'POST'])
+def verify():
+   if request.method == 'GET':
+       pass
+   else:
+       name = request.form.get('name')
+       number = request.form.get('number')
+       pin = request.form.get('pin')
+       if not all((name, number, pin)):
+           flash("Please enter all required details.")
+       else:
+           print(f"You clicked the button with name {name}, number {number} and PIN {pin}!")
+           verify_number(name, number, pin)
+   return render_template('verify.html')
+
+def verify_number(name, number, pin):
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute(f"""SELECT verification_code from people
+                WHERE name = '{name}' AND phone_number = {number}""")
+    pin_id = cur.fetchone()[0]
+    channel = SMSChannel.from_env()
+    response = channel.verify_phone_number(
+        pin_id,
+        {"pin": f"{pin}"}
+    )
+    if response.verified:
+        cur.execute(f"""
+            UPDATE people
+            SET verified = 1
+            WHERE phone_number = {number};"""
+        )
+        conn.commit()
+        flash(f"Your number has been verified!")
+    else:
+        flash(f"Error: Number not verified: {response.pin_error}")
+    conn.close()
---
+++ set_up_2fa_application.py
@@ -0,0 +1,38 @@
+from infobip_channels.sms.channel import SMSChannel
+
+channel = SMSChannel.from_env()
+
+response = channel.create_tfa_application(
+    {
+    "name": "gift exchange 2fa application",
+    "enabled": "true",
+    "configuration": {
+        "pinAttempts": 7,
+        "allowMultiplePinVerifications": "true",
+        "pinTimeToLive": "11m",
+        "verifyPinLimit": "2/4s",
+        "sendPinPerApplicationLimit": "5000/12h",
+        "sendPinPerPhoneNumberLimit": "2/1d",}
+    }
+
+)
+
+response = channel.get_tfa_applications()
+application_id = response.list[0].application_id
+print(application_id)
+
+response = channel.create_tfa_message_template(
+    application_id,
+    {
+            "pinType": "NUMERIC",
+            "pinPlaceholder": "{{pin}}",
+            "messageText": "Your pin is {{pin}}",
+            "pinLength": 4,
+            "language": "en",
+            "senderId": "Infobip 2FA",
+            "repeatDTMF": "1#",
+            "speechRate": 1,
+    }
+)
+template_id = response.list[0].message_id
+print(template_id)
